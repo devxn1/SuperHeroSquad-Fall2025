34c3dd4aaef5476893d0afe8d5bc1b330b4a6c4c
Clark <jnewell1@ggc.edu>
2025-10-13 10:27:55 -0400

    Added Subtraction method -ClarkN

--- src/Game.java
+++ src/Game.java
@@ -10,5 +10,12 @@ public class Game {
         int a=10;
         int b=9;
         System.out.println(a+b);
+
+    }
+
+    public void Subtraction() {
+        int z = 10000;
+        int x = -9999;
+        System.out.println(z+x);
     }
 }
