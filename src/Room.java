047b5deae0d4ad34bb824bb26e8e5af5a3dcd0a3
Clark <jnewell1@ggc.edu>
2025-11-12 10:37:50 -0500

    Room: Added visit, bolean for visit
    Player: Added stats command, added Help command, added gettersetter HP (speaking of which why do we have a player AND a character class?)
    
    Character: set hp to 100, should we delete this class?

--- src/Room.java
+++ src/Room.java
@@ -7,8 +7,9 @@ public class Room {
     private String South;
     private String West;
     private Puzzle puzzle;
+    private boolean visited;
 
-    public Room(int roomLocation, String roomName, String roomDescription, String North, String East, String South, String West) {
+    public Room(int roomLocation, String roomName, String roomDescription, String North, String East, String South, String West, Boolean visited) {
         this.roomLocation = roomLocation;
         this.roomName = roomName;
         this.roomDescription = roomDescription;
@@ -16,6 +17,7 @@ public class Room {
         this.East = East;
         this.South = South;
         this.West = West;
+        this.visited = false;
     }
 
     public Puzzle getPuzzle() {
@@ -33,4 +35,7 @@ public class Room {
 
     }
 
+    public void visit() {
+        visited = true;
+    }
 }
